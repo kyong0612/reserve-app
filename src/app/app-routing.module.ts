16e61b6ab62aa656f80d007cead90a9b16a07c9b
KIm <fenderst0612@gmail.com>
2020-06-30 21:55:50 +0900

    product-detail.component追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductListComponent } from './common/product-list/product-list.component';
+import { ProductDetailComponent } from './common/product-detail/product-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ProductListComponent },
+  { path: 'detail', component: ProductDetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
-import { ProductListComponent } from './common/product-list/product-list.component'
+import { ProductListComponent } from './common/product-list/product-list.component';
+import { ProductDetailComponent } from './common/product-detail/product-detail.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProductListComponent
+    ProductListComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
