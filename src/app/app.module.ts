ea4e1b463686a6b4ac5a2102c503a75aeb190576
KIm <fenderst0612@gmail.com>
2020-07-01 23:19:40 +0900

    product.module追加中

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './common/navbar/navbar.component';
 import { ProductListComponent } from './common/product-list/product-list.component';
-import { ProductDetailComponent } from './common/product-detail/product-detail.component'
+import { ProductDetailComponent } from './common/product-detail/product-detail.component';
+import { ProductComponent } from './common/product/product.component'
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ProductDetailComponent } from './common/product-detail/product-detail.c
     AppComponent,
     NavbarComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/common/product/product.comonent.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from '../product-list/product-list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+
+
+  ],
+  imports: [
+
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class ProductModule { }
