5fd37c1d31dd1427182cdc4b5cb9878e7a7caa1f
KIm <fenderst0612@gmail.com>
2020-07-05 16:25:53 +0900

    ・商品詳細ページへの遷移作成 ・商品一覧にて同じ名前の商品が表示される仕様改善

--- src/app/common/product/product-detail/product-detail.component.ts
+++ src/app/common/product/product-detail/product-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { products } from '../../../products';
+
 
 @Component({
   selector: 'app-product-detail',
@@ -8,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 export class ProductDetailComponent implements OnInit {
   product: any;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.product = products[+params.get('productId')];
+
+    });
   }
 
 }
--- src/app/common/product/product.module.ts
+++ src/app/common/product/product.module.ts
@@ -1,23 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
 
 import { ProductListComponent } from './product-list/product-list.component';
 import { AppComponent } from '../../app.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { AppRoutingModule } from 'src/app/app-routing.module';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
 
 
 const routes: Routes = [{
-    path: 'products', component: ProductComponent,
-    children: [
-      { path: '', component: ProductListComponent },
-      { path: 'detail', component: ProductDetailComponent }
-    ]
-  }]
-  
+  path: 'products', component: ProductComponent,
+  children: [
+    { path: '', component: ProductListComponent },
+    { path: ':productId', component: ProductDetailComponent }
+  ]
+}]
+
 
 @NgModule({
   declarations: [
