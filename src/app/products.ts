f79cd2ec359fc5fa94729b0ab368e9c6e2164957
KIm <fenderst0612@gmail.com>
2020-07-07 22:46:09 +0900

    画像を動的に更新

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,17 +2,42 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'sample heading 1',
+    heading2: 'sample heading 2',
+    heading3: 'sample heading 3',
+    coverimg: "./assets/img/phone-cover.jpg",
+    topimg:"https://picsum.photos/100/100"
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'sample heding 1',
+    heading2: 'sample heading 2',
+    heading3: 'sample heading 3',
+    coverimg: "./assets/img/phone-cover.jpg",
+    topimg: "https://picsum.photos/100/100"
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: 'サンプルテキスト',
+    heading1: 'サンプルテキスト',
+    heading2: 'サンプルテキスト',
+    heading3: 'サンプルテキスト',
+    coverimg: "./assets/img/phone-cover.jpg",
+    topimg: "https://picsum.photos/100/100"
+  },
+  {
+    name: 'Phone special',
+    price: 999,
+    description: 'this is the special,amazing,phone',
+    heading1: 'sample heding 1',
+    heading2: 'sample heading 2',
+    heading3: 'sample heading 3',
+    coverimg: "./assets/img/phone-cover.jpg",
+    topimg: "https://picsum.photos/100/100"
   }
 ];
 
