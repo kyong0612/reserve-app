e2dbceea10114456cb3fce3ede792afb2b4e24d0
KIm <fenderst0612@gmail.com>
2020-07-09 21:00:45 +0900

    ・デプロイ環境適応設定 ・エンドポイントの設定

--- server/index.js
+++ server/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const app = express()
 
+app.get('/products', function (req, res) {
+  res.json({
+    'success': true
+  })
+})
+
 const PORT = process.env.PORT || '3001'
 
-app.listen('3001', function () {
+app.listen(PORT, function () {
   console.log('I am running')
 })
