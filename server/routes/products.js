c646e7c4e391f63c7b1893b6f0e864793c38e7d8
KIm <fenderst0612@gmail.com>
2020-07-12 17:33:01 +0900

    エラーハンドラー追加

--- server/routes/products.js
+++ server/routes/products.js
@@ -6,14 +6,17 @@ const Product = require('../model/product')
 
 router.get('', function (req, res) {
   Product.find({}, function (err, fundProducts) {
-    res.json(fundProducts)
+    return res.json(fundProducts)
   })
 })
 
 router.get('/:productId', function (req, res) {
   const productId = req.params.productId
   Product.findById(productId, function (err, fundProduct) {
-    res.json(fundProduct)
+    if (err) {
+      return res.status(422).send({ erros: [{ title:'Product erro' ,detail:'Product not found!'  }]})
+    }
+    return res.json(fundProduct)
   })
 })
 
