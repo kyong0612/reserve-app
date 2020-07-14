8978df74a046d82ef68e34aff867a5f715ee01a6
KIm <fenderst0612@gmail.com>
2020-07-14 15:22:17 +0900

    ２点修正 ・起動時にページを自動で開く ・一覧ページで画像が表示されないエラー解消

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProductListComponent } from './common/product/product-list/product-list.component';
-import { ProductDetailComponent } from './common/product/product-detail/product-detail.component';
 import { ProductModule } from './common/product/product.module';
-import { ProductComponent } from './common/product/product.component';
 
 
 const routes: Routes = [
